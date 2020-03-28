5d6027d967e4683696f0b19ea8bda0017d7ed64b
Alexey Fadeev <fadeev1087@gmail.com>
2020-03-28 16:27:30 +0300

    DS3231 with alarm

--- DS3231/DS3231_alarm.ino
+++ DS3231/DS3231_alarm.ino
@@ -15,33 +15,25 @@ int alarmHour = 8;
 
 void setup()
 {
-    Serial.begin(9600);
-
     // Инициализация DS3231
     clock.begin();
 
-    // устанавливаем в качестве текущее время время компиляции скетча
+    // устанавливаем в качестве текущего время время компиляции скетча
     clock.setDateTime(__DATE__, __TIME__);
 }
 
 void loop()
 {
+    // получаем текущее время
     dt = clock.getDateTime();
-
     seconds = dt.second;
     minutes = dt.minute;
     hours = dt.hour;
 
-    // выводим время
-    Serial.print("Time: ");
-    Serial.print(hours); Serial.print(":");
-    Serial.print(minutes); Serial.print(":");
-    Serial.print(seconds); Serial.println("");
-
     // проверяем будильник, время срабатывания - 8:00:00
     if (hours == alarmHour && minutes == alarmMinute && seconds == alarmSecond)
     {
-        Serial.println("Alarm is armed");
+        // TO DO
     }
     delay(1000);
 }
